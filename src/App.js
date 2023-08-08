12339f4e849d56f3720249f2ecf7404aff122f50
Isaque Lopes <isaque.lopes047@gmail.com>
2023-08-08 11:11:23 -0300

    Estilizando o meu superior

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
+import Logo from './components/Logo';
+import perfil from './images/perfil.svg';
+import sacola from './images/sacola.svg';
 
-import Logo from './components/Logo'
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
+const icones = [perfil, sacola]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Logo />
+
+        <ul className='opcoes'>
+          { textoOpcoes.map((texto, index) => {
+            return (
+              <li className='opcao' key={index}>
+                <p> {texto} </p>
+              </li>
+            )
+          } )}
+        </ul>
+
+        <ul className='icones'>
+          { icones.map((icone, index) => {
+            return (
+              <li className='icone'>
+                <img 
+                  src={icone} 
+                  key={index}
+                  alt='Icones Menu'
+                />
+              </li>
+            )
+          })}
+        </ul>
+        
       </header>
     </div>
   );
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -7,6 +7,7 @@ export default function Logo() {
             <img
                 src={logo}
                 alt='logo'
+                className={styles.logoimg}
             />
             <p>Alura Books</p>
         </div>
